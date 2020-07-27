166e8f4a5031fd4659496292f72683a6a85aea7c
wanggang <wanggang@jinghang.ai>
2020-07-27 18:45:59 +0800

    add some code

---
+++ build/schema.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+import { DEFAULT_OPTIONS, compileFromFile } from 'json-schema-to-typescript';
+import path from 'path';
+import fs from 'fs';
+
+DEFAULT_OPTIONS.bannerComment = '/**\n * DO NOT MODIFY IT BY HAND.\n * Generator it with command: `npm run schema`.\n */';
+DEFAULT_OPTIONS.style.singleQuote = true;
+DEFAULT_OPTIONS.style.tabWidth = 4;
+
+const schemaFolder = path.resolve(__dirname, '..', 'schema');
+
+async function findFile(folder = schemaFolder) {
+    const files = fs.readdirSync(folder);
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const currFolder = path.join(folder, file);
+        const isFile = fs.statSync(currFolder).isFile();
+        if (isFile) {
+            await compile(currFolder);
+        } else {
+            await findFile(currFolder);
+        }
+    }
+}
+
+async function compile(filePath: string) {
+    if (filePath.match(/.json$/)) {
+        const ts = await compileFromFile(filePath, DEFAULT_OPTIONS);
+        const outPath = getOutPath(filePath);
+        fs.writeFileSync(outPath, ts);
+    }
+}
+
+function getOutPath(filePath: string): string {
+    const outFolder = path.resolve(__dirname, '..', 'src', '@types');
+    const outPath = filePath.replace(schemaFolder, outFolder).replace(/.json$/, '.d.ts');
+    if (!fs.existsSync(outPath)) {
+        fs.mkdirSync(outPath.replace(path.basename(outPath), ''));
+    }
+    return outPath;
+}
+
+findFile(schemaFolder);
---
+++ src/@types/parameter/test.d.ts
@@ -0,0 +1,14 @@
+/**
+ * DO NOT MODIFY IT BY HAND.
+ * Generator it with command: `npm run schema`.
+ */
+
+export interface ExampleSchema {
+    firstName: string;
+    lastName: string;
+    /**
+     * Age in years
+     */
+    age?: number;
+    hairColor?: 'black' | 'brown' | 'blue';
+}
